cd3e8ae36d2c0b6af1c3895daaeb61d5b56bfbf9
Mark <mark.maslar@blackpants.com>
2019-03-19 19:58:59 -0400

    03192019c

--- 1_start_ganache-cli - Local.sh
+++ 1_start_ganache-cli - Local.sh
@@ -1,3 +1,3 @@
 cd "C:\Users\Mark\source\repos\GitHub\flightsurety\contracts"
-ganache-cli -m "spirit supply whale amount human item harsh scare congress discover talent hamster"
+ganache-cli -m "spirit supply whale amount human item harsh scare congress discover talent hamster" -a 20
 /bin/bash
